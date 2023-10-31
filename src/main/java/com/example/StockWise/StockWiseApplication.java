68e3fca78f7162439986d620a69b9e7432236cfa
kevin changani <changanikevin96@gmail.com>
2023-10-31 18:14:12 +0530

    Spring Intilizr did

---
+++ src/main/java/com/example/StockWise/StockWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockWiseApplication.class, args);
+	}
+
+}
