1fe555c3a7d29ccb232af5901cf1625e176efbb8
kevin changani <changanikevin96@gmail.com>
2023-11-01 12:07:40 +0530

    Create login and signup system

--- src/main/java/com/example/StockWise/Controller/UserController.java
+++ src/main/java/com/example/StockWise/Controller/UserController.java
@@ -1,5 +1,33 @@
 package com.example.StockWise.Controller;
 
+import com.example.StockWise.Model.User;
+import com.example.StockWise.Model.dto.Credential;
+import com.example.StockWise.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequestMapping("/user")
+//@Tag(name = "Greeting", description = "Endpoints for greeting messages")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/signup")
+    private String registerUser(@RequestBody User user) throws NoSuchAlgorithmException {
+        return userService.registerUser(user);
+    }
+
+    @GetMapping("/login")
+    private String loginUser(@RequestBody Credential credential) throws NoSuchAlgorithmException {
+        return userService.loginUser(credential);
+    }
+
+    @GetMapping("/logout")
+    private String signoutUser(@RequestParam String email) throws NoSuchAlgorithmException {
+        return userService.signoutUser(email);
+    }
 
 }
---
+++ src/main/java/com/example/StockWise/Services/UserService.java
@@ -0,0 +1,59 @@
+package com.example.StockWise.Services;
+
+import com.example.StockWise.Model.User;
+import com.example.StockWise.Model.dto.Credential;
+import com.example.StockWise.Repository.UserRepo;
+import com.example.StockWise.Services.Utility.PasswordEncrypter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepo userRepo;
+
+    public String registerUser(User user) throws NoSuchAlgorithmException {
+
+        if (userRepo.existsByuserEmail(user.getUserEmail())){
+            return "Already Register";
+        }
+        String hashPass = PasswordEncrypter.hashPasswordWithStaticSecret(user.getUserPassword());
+        user.setUserPassword(hashPass);
+        userRepo.save(user);
+        return "Register Successfully";
+    }
+
+    public String loginUser(Credential credential) throws NoSuchAlgorithmException {
+        if (!userRepo.existsByuserEmail(credential.getEmail())){
+            return "Please Create a Account";
+        }
+        String hashPass = PasswordEncrypter.hashPasswordWithStaticSecret(credential.getPassword());
+
+        User user = userRepo.findByUserEmail(credential.getEmail());
+
+
+        if (hashPass.equals(user.getUserPassword())) {
+            user.setStatus("logged in");
+            userRepo.save(user);
+            return "login successfully";
+        } else {
+            return "Credential MisMatch";
+        }
+    }
+
+    public String signoutUser(String email) {
+        User user = userRepo.findByUserEmail(email);
+
+        if (user.getStatus().equals("logged in")){
+            user.setStatus("log out");
+            userRepo.save(user);
+            return "logout successfully";
+        }
+        else {
+            return "plz login first then click on logout ";
+        }
+
+    }
+}
